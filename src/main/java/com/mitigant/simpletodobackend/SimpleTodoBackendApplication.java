709a5a47e778682cb0c5e8368876a6216a2bc456
Shiva <tdp511a@gmail.com>
2023-03-12 08:59:04 +0100

    commiting initial spring starter code

---
+++ src/main/java/com/mitigant/simpletodobackend/SimpleTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mitigant.simpletodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTodoBackendApplication.class, args);
+	}
+
+}
